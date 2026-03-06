ce3c04a4cb47f676b44fe262e042db10deb0b697
AmitBholaOPS <chigwel.family@gmail.com>
2026-03-06 03:02:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'amit-pa-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const ASSETS = ['/amit-pa/', '/amit-pa/index.html', '/amit-pa/manifest.json', '/amit-pa/icons/icon-192.png', '/amit-pa/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
